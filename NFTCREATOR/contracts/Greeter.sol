2dd3c44c38eb8e830cd27b56370ba3034746b972
mananjabrik <manan.jabrik@gmail.com>
2022-05-22 14:40:11 +0700

    add open zepline

--- NFTCREATOR/contracts/Greeter.sol
+++ NFTCREATOR/contracts/Greeter.sol
@@ -1,22 +1,9 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Greeter {
-    string private greeting;
-
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
-
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
-    }
+/// @custom:security-contact manan.jabrik@gmail.coim
+contract GaleriNFT is ERC721 {
+    constructor() ERC721("GaleriNFT", "GNFT") {}
 }
