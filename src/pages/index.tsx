0451be216109ea4383ff74b6d10d01c994654d3b
mananjabrik <manan.jabrik@gmail.com>
2022-05-24 02:43:13 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import type { NextPage } from 'next';
-import { ethers } from 'ethers';
+import { ethers, utils } from 'ethers';
 import axios from 'axios';
 import {
   Box,
@@ -16,30 +16,74 @@ import {
 import { abi } from '../components/abi';
 import { ListCard } from '../components';
 
+// const abi = [
+//   'function verifyString(string, uint8, bytes32, bytes32) public pure returns (address)',
+// ];
+
+// const provider = ethers.getDefaultProvider('ropsten');
+// const contractAddress = '0x80F85dA065115F576F1fbe5E14285dA51ea39260';
+//   const contract = new ethers.Contract(contractAddress, abi, provider);
+
+// export const verifySignature = async (
+//   message: string,
+//   flatSig: string,
+//   address: string
+// ) => {
+//   try {
+//     const sig = ethers.utils.splitSignature(flatSig);
+//     const recovered = await contract.verifyString(message, sig.v, sig.r, sig.s);
+//     return compareAddress(recovered, address);
+//   } catch (error) {
+//     return false;
+//   }
+// };
+
+// export const getWalletAddressBySig = async (
+//   message: string,
+//   flatSig: string
+// ) => {
+//   try {
+//     const sig = ethers.utils.splitSignature(flatSig);
+//     const recovered = await contract.verifyString(message, sig.v, sig.r, sig.s);
+//     return recovered;
+//   } catch (error) {
+//     return '';
+//   }
+// };
+
 const Home: NextPage = () => {
   const contracAddressRef = useRef<HTMLInputElement>(null);
   const apiUrl = useRef<HTMLInputElement>(null);
   const [state, setState] = useState('');
   const [nfts, setNfts] = useState<any>([]);
+  const [myWallet, setMyWalet] = useState('');
   const provider = new ethers.providers.JsonRpcProvider(
     'https://bsc-dataseed1.ninicoin.io/'
   );
+
+  const connectWallet = async () => {
+    try {
+      const detectProvider = new ethers.providers.Web3Provider(
+        //@ts-ignore
+        window.ethereum,
+        'any'
+      );
+      const addressWallet = await detectProvider.send(
+        'eth_requestAccounts',
+        []
+      );
+      setMyWalet(addressWallet[0]);
+    } catch {
+      console.log;
+    }
+  };
   const useNftList = async (contract: string) => {
     const nftContract = new ethers.Contract(contract, abi, provider);
     try {
-      const countNft = await nftContract.balanceOf(
-        '0x664FD97eBB060ab4C02C90C6d8772D80A846C7F3'
-      );
-      const myList = await nftContract.tokenOfOwnerByIndex(
-        '0x664FD97eBB060ab4C02C90C6d8772D80A846C7F3',
-        1
-      );
+      const countNft = await nftContract.balanceOf(myWallet);
       if (countNft) {
         for (let i = 0; i < countNft; i++) {
-          const nft = await nftContract.tokenOfOwnerByIndex(
-            '0x664FD97eBB060ab4C02C90C6d8772D80A846C7F3',
-            i
-          );
+          const nft = await nftContract.tokenOfOwnerByIndex(myWallet, i);
           const getApiUrl = apiUrl.current?.value!;
           const { data: metaData } = await axios.get(`${getApiUrl}${nft}`);
           setNfts((prev: any) => [...prev, metaData]);
@@ -49,7 +93,6 @@ const Home: NextPage = () => {
       console.log;
     }
   };
-  console.log(nfts);
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const address = contracAddressRef.current?.value!;
@@ -61,6 +104,7 @@ const Home: NextPage = () => {
 
   return (
     <Box>
+      <Button onClick={connectWallet}>{myWallet ? myWallet : 'Connect'}</Button>
       <Container maxW="container.md" py="1rem">
         <Heading textAlign="center">Galeri My NFTS</Heading>
         <Box py="1rem">
@@ -85,8 +129,6 @@ const Home: NextPage = () => {
               <Button type="submit">Submit</Button>
             </Box>
           </form>
-
-          <Text>{state}</Text>
         </Box>
 
         <Box py="1rem">
